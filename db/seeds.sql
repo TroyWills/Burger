ce895e1e4ae0f2a32a4eab6ed23a5853b4a21671
Troy Wills <troy_wills@outlook.com>
2021-02-10 22:02:05 -0600

    initial

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name, devoured) VALUES ('Hamburger', 1)
+INSERT INTO burgers (name, devoured) VALUES ('Cheeseburger', 1)
+INSERT INTO burgers (name, devoured) VALUES ('Bacon Cheeseburger', 1)
+INSERT INTO burgers (name, devoured) VALUES ('Mushroom Swiss', 1)
+
