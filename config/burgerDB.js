5cf40f2df3dcce1295ef456e8200e62da07580bb
Troy Wills <troy_wills@outlook.com>
2021-03-19 09:19:46 -0500

    mysql connection to DB

---
+++ config/burgerDB.js
@@ -0,0 +1,19 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+  user: "root",
+  password: "root"
+  database: "burger"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  readColleges();
+});
+
+
--- orm/connection.js
+++ orm/connection.js
@@ -1,5 +1,8 @@
+// This file is not currently connected to any part of the application -- saving in-case needed
+
 // Dependencies
 var Sequelize = require("sequelize");
+
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
 const sequelize = new Sequelize('burger', 'root', 'root', {
   host: 'localhost',
