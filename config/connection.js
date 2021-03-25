1e126d4780105c6dbb4af3c4f147933bf355a113
Troy Wills <troy_wills@outlook.com>
2021-03-24 20:18:29 -0500

    another heroku try

--- config/connection.js
+++ config/connection.js
@@ -1,31 +1,14 @@
-var mysql = require("mysql");
-
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "root",
-//   database: "burger"
-// });
+const mysql = require("mysql");
 
 const connectionString = process.env.JAWSDB_URL || {
-  host: "pxukqohrckdfo4ty.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  user: "cxrz336zg2x6nfb2",
-  password: "ofbclzozwxpios27",
-  database: "h3ohs6fl8slkfor5",
-  PORT: "3306"
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "burger"
 }
 
 const connection = mysql.createConnection(connectionString);
 
 connection.connect();
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
 
 module.exports = connection;
